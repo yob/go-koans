fca74192b016841a7dc7e5381659d52ec34338df
James Healy <jimmy@deefa.com>
2012-09-22 18:29:30 +0100

    gofmt

--- about_types.go
+++ about_types.go
@@ -3,11 +3,11 @@ package go_koans
 type coolNumber int
 
 func (cn coolNumber) multiplyByTwo() int {
-  return int(cn) * 2
+	return int(cn) * 2
 }
 
 func aboutTypes() {
-  i := coolNumber(4)
-  assert(i == coolNumber(4)) // values can be converted between compatible types
-  assert(i.multiplyByTwo() == 8)    // you can add methods on any type you define
+	i := coolNumber(4)
+	assert(i == coolNumber(4))     // values can be converted between compatible types
+	assert(i.multiplyByTwo() == 8) // you can add methods on any type you define
 }
