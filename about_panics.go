912fee9e95c0bb146ce9979ffa7efa7dcc408a2d
James Healy <jimmy@deefa.com>
2012-09-24 17:56:04 +0100

    gofmt love

--- about_panics.go
+++ about_panics.go
@@ -1,13 +1,13 @@
 package go_koans
 
 func divideFourBy(i int) int {
-  return 4 / i
+	return 4 / i
 }
 
 const __divisor__ = 2
 
 func aboutPanics() {
 
-  n := divideFourBy(__divisor__)
-  assert(n == 2) // panics are exceptional errors at runtime
+	n := divideFourBy(__divisor__)
+	assert(n == 2) // panics are exceptional errors at runtime
 }
