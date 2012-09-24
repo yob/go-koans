1eacd45fb6039090b2b05acc7a73a8d871da10b7
James Healy <jimmy@deefa.com>
2012-09-24 13:24:27 +0100

    gofmt love

--- about_channels.go
+++ about_channels.go
@@ -3,29 +3,29 @@ package go_koans
 import "fmt"
 
 func aboutChannels() {
-  ch := make(chan string, 2)
+	ch := make(chan string, 2)
 
-  assert(len(ch) == 0) // channels are like buffers
+	assert(len(ch) == 0) // channels are like buffers
 
-  ch <- "foo" // i mean, "metaphors are like similes"
+	ch <- "foo" // i mean, "metaphors are like similes"
 
-  assert(len(ch) == 1) // they can be queried for queued items
+	assert(len(ch) == 1) // they can be queried for queued items
 
-  assert(<-ch == "foo") // items can be popped out of them
+	assert(<-ch == "foo") // items can be popped out of them
 
-  assert(len(ch) == 0) // and len() always reflects the "current" queue status
+	assert(len(ch) == 0) // and len() always reflects the "current" queue status
 
-  // the 'go' keyword runs a function-call in a new "goroutine"
-  // which executes "concurrently" with the calling "goroutine"
-  go func() {
-    for i := 0; i < 3; i++ {
-      fmt.Print(<-ch, "\n")
-    }
-  }()
+	// the 'go' keyword runs a function-call in a new "goroutine"
+	// which executes "concurrently" with the calling "goroutine"
+	go func() {
+		for i := 0; i < 3; i++ {
+			fmt.Print(<-ch, "\n")
+		}
+	}()
 
-  assert(cap(ch) == 2) // we'll need to make room for the queue, or suffer deadlocks
+	assert(cap(ch) == 2) // we'll need to make room for the queue, or suffer deadlocks
 
-  ch <- "bar"   // this send will succeed
-  ch <- "quux"  // there's enough room for this send too
-  ch <- "extra" // but the buffer only has two slots
+	ch <- "bar"   // this send will succeed
+	ch <- "quux"  // there's enough room for this send too
+	ch <- "extra" // but the buffer only has two slots
 }
