49311ed7378155f68bb3ce0c6274c8a8a0655385
James Healy <jimmy@deefa.com>
2012-09-24 17:50:59 +0100

    gofmt love

--- about_concurrency.go
+++ about_concurrency.go
@@ -1,31 +1,31 @@
 package go_koans
 
 func isPrimeNumber(possiblePrime int) bool {
-  for underPrime := 2; underPrime < possiblePrime; underPrime++ {
-    if possiblePrime % underPrime == 0 {
-      return false
-    }
-  }
-  return true
+	for underPrime := 2; underPrime < possiblePrime; underPrime++ {
+		if possiblePrime%underPrime == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func findPrimeNumbers(channel chan int) {
-  for i := 2 ; i < 100 ; i++ {
-    if isPrimeNumber(i) {
-      channel <- i
-    }
-    assert(i < 100) // i is afraid of heights
-  }
+	for i := 2; i < 100; i++ {
+		if isPrimeNumber(i) {
+			channel <- i
+		}
+		assert(i < 100) // i is afraid of heights
+	}
 }
 
 func aboutConcurrency() {
-  ch := make(chan int)
+	ch := make(chan int)
 
-  go findPrimeNumbers(ch)
+	go findPrimeNumbers(ch)
 
-  assert(<-ch == 2)
-  assert(<-ch == 3)
-  assert(<-ch == 5)
-  assert(<-ch == 7)
-  assert(<-ch == 11)
+	assert(<-ch == 2)
+	assert(<-ch == 3)
+	assert(<-ch == 5)
+	assert(<-ch == 7)
+	assert(<-ch == 11)
 }
